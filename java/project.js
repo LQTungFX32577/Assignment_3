1610da6cfbe0cabec1738fb3767b60b8d2313b5b
LQTungFX32577 <159262917+LQTungFX32577@users.noreply.github.com>
2024-02-19 18:16:29 +0700

    Update project.js

--- java/project.js
+++ java/project.js
@@ -1,5 +1,5 @@
 'use strict';
 //back from project page demo
 document.body.addEventListener('click', function(){
-location.href = "http://127.0.0.1:5500/#certificate"
+location.href = "https://lqtungfx32577.github.io/Assignment_3/"
 })
