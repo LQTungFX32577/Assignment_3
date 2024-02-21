7e6e561a5dff27d3401aa28c2077debae29494c3
LQTungFX32577 <159262917+LQTungFX32577@users.noreply.github.com>
2024-02-21 17:39:38 +0700

    Update project.js

--- java/project.js
+++ java/project.js
@@ -1,5 +1,5 @@
-'use strict';
+//'use strict';
 //back from project page demo
-document.body.addEventListener('click', function(){
-location.href = "https://lqtungfx32577.github.io/Assignment_3/";
-})
+//document.body.addEventListener('click', function(){
+//location.href = "https://lqtungfx32577.github.io/Assignment_3/";
+//})
